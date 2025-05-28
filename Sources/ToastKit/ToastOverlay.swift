f4e9fe7cfe4d88a0b4ac1a5b4fc0e5d6192965ca
Andy Kale <andy@redpiston.com>
2025-05-27 21:10:45 -0400

    cleaned up commented out code

--- Sources/ToastKit/ToastOverlay.swift
+++ Sources/ToastKit/ToastOverlay.swift
@@ -62,48 +62,5 @@ public struct ToastOverlay: View {
             }
         }
     }
-    
-//    public var body: some View {
-//        if let message = toastManager.message {
-//            if toastManager.
-//            HStack(spacing: 10) {
-//                Image(systemName: toastManager.type.iconName)
-//                    .foregroundColor(toastManager.type.backgroundColor)
-//
-//                Text(message)
-//                    .font(.callout)
-//                    .foregroundColor(.primary)
-//                    .bold()
-//            }
-//            .padding(.horizontal, 16)
-//            .padding(.vertical, 10)
-//            .background(Color(UIColor.systemBackground))
-//            .cornerRadius(14)
-//            .shadow(radius: 8)
-//            .transition(.move(edge: .bottom).combined(with: .opacity))
-//            .padding(.top, 60)
-//            .zIndex(1)
-//            .frame(maxHeight: .infinity, alignment: .bottom)
-//            .animation(.easeInOut(duration: 0.3), value: toastManager.message)
-//            //            VStack {
-//            //                Spacer()
-//            //                HStack(spacing: 12) {
-//            //                    Image(systemName: toastManager.type.iconName)
-//            //                        .foregroundColor(.white)
-//            //
-//            //                    Text(message)
-//            //                        .foregroundColor(.white)
-//            //                        .multilineTextAlignment(.leading)
-//            //                }
-//            //                .padding()
-//            //                .background(toastManager.type.backgroundColor)
-//            //                .cornerRadius(12)
-//            //                .padding(.bottom, 40)
-//            //                .shadow(radius: 5)
-//            //                .transition(.move(edge: .bottom).combined(with: .opacity))
-//            //                .animation(.easeInOut(duration: 0.3), value: toastManager.message)
-//            //            }
-//        }
-//    }
 }
 #endif
